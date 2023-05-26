1fff3790da1863c36c6c30e5cfabdd4157a6450d
Laptop (Brent) <124935518+Brent-Bew-Canon@users.noreply.github.com>
2023-05-25 21:04:24 -0700

    test js

--- public/js/display.js
+++ public/js/display.js
@@ -1,5 +1,7 @@
 const movBtn = document.querySelector("#saveMovie")
+const movBtn2 = document.querySelector("button")
 
-movBtn.addEventListener("click", () => {
+movBtn2.addEventListener("click", () => {
     console.log("hello")
 })
+
