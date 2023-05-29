b9b6a049ae116d136e1d19b57271f1a61c558361
Brent <124935518+Brent-Bew-Canon@users.noreply.github.com>
2023-05-29 12:08:36 -0700

    save button outputs the movie details to console

--- public/js/display.js
+++ public/js/display.js
@@ -3,6 +3,9 @@ const movBtn = document.querySelectorAll("#saveMovie")
 // adds event listener to each button
 movBtn.forEach(btn => {
     btn.addEventListener("click", () => {
-        console.log(btn.parentElement.firstChild)
+        myEl = btn.parentElement
+        // myEl = btn.nextElementSibling
+        console.log(myEl);
+        console.log(myEl.firstChild.nextSibling.firstChild.nextSibling.innerText)
     })
 });
