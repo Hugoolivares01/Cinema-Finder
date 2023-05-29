f26035edb7cdbb891a6f07a7b5a8a378e1f8294a
Brent <124935518+Brent-Bew-Canon@users.noreply.github.com>
2023-05-29 13:03:23 -0700

    created savedmovies seeds file and rendering

--- public/js/display.js
+++ public/js/display.js
@@ -1,11 +1,24 @@
 const movBtn = document.querySelectorAll("#saveMovie")
+const delBtn = document.querySelectorAll("#deleteMovie")
 
-// adds event listener to each button
+// adds event listener to each save movie button
 movBtn.forEach(btn => {
     btn.addEventListener("click", () => {
         myEl = btn.parentElement
-        // myEl = btn.nextElementSibling
-        console.log(myEl);
         console.log(myEl.firstChild.nextSibling.firstChild.nextSibling.innerText)
     })
 });
+
+// adds event listener to each save delete button
+delBtn.forEach(btn => {
+    btn.addEventListener("click", () => {
+        myEl = btn.parentElement
+        console.log(myEl.firstChild.nextSibling.firstChild.nextSibling.innerText)
+    })
+});
+
+// TODOS:
+// 1. get the id of the correct element
+// 2. query the database to get all the other information related to the movie
+// 3. add the saved movie to the saved movie table
+// 4. render the movie information from the SQL query as a new element under the SavedMovies List
