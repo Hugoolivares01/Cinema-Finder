e9769a91de1247b26060e2b264e729a178b77a81
Brent <124935518+Brent-Bew-Canon@users.noreply.github.com>
2023-06-01 15:33:44 -0700

    Model Associations correctly made

--- models/index.js
+++ models/index.js
@@ -2,12 +2,13 @@ const User = require('./User');
 const MovieList = require('./MovieList');
 const SavedMovies = require('./SavedMovies');
 
-SavedMovies.belongsTo(User, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
-});
 
-MovieList.belongsTo(User, {
-  foreignKey: 'user_id'
-});
+User.hasMany(SavedMovies, { foreignKey: 'user_id' });
+
+// SavedMovies.belongsTo(User, { foreignKey: 'userId' });
+// SavedMovies.belongsTo(MovieList, { foreignKey: 'MovieListId' });
+// MovieList.belongsToMany(User, { through: SavedMovies });
+// User.belongsToMany(SavedMovies, { through: 'UserMovies' })
+// SavedMovies.belongsToMany(User, { through: 'UserMovies' });
+
 module.exports = { User, MovieList, SavedMovies };
