0db27d899ee0328d41b0914a82dece02dcc7245d
mubarak <mubarakcali24@gmail.com>
2023-06-01 19:35:05 -0700

    movie review

--- public/js/display.js
+++ public/js/display.js
@@ -1,41 +1,59 @@
 // const { json } = require("express");
 
-const movBtn = document.querySelectorAll("#saveMovie")
-const delBtn = document.querySelectorAll("#deleteMovie")
-const movRvw = document.querySelectorAll("movieReview")
-let id = 0
+const movBtn = document.querySelectorAll("#saveMovie");
+const delBtn = document.querySelectorAll("#deleteMovie");
+const reviewForms = document.querySelectorAll(".reviewForm");
+let id = 0;
 
 // adds event listener to each save movie button
-movBtn.forEach(btn => {
-    btn.addEventListener("click", async function () {
-        myEl = btn.parentElement
-        // console.log(myEl.firstChild.nextSibling.firstChild.nextSibling.innerText)
-        id = (myEl.firstChild.nextSibling.firstChild.nextSibling.nextSibling.nextSibling.innerText)
-        console.log(id)
+movBtn.forEach((btn) => {
+  btn.addEventListener("click", async function () {
+    myEl = btn.parentElement;
+    id = myEl.querySelector(".movieId").innerText;
+    console.log(id);
 
-        try {
-            response = await fetch('/api/save', {
-                method: 'POST',
-                body: JSON.stringify({ id }),
-                headers: { 'Content-Type': 'application/json' },
-            });
-            document.location.reload()
-            // console.log(response)
-        } catch (error) {
-            console.log(error)
-        }
-    })
+    try {
+      const response = await fetch("/api/save", {
+        method: "POST",
+        body: JSON.stringify({ id }),
+        headers: { "Content-Type": "application/json" },
+      });
+      document.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
+  });
+});
+
+reviewForms.forEach((form) => {
+  form.addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    const movieName = form.querySelector(".movieName").value;
+    const review = form.querySelector(".review").value;
+
+    try {
+      const response = await fetch("/api/save", {
+        method: "UPDATE",
+        body: JSON.stringify({ movieName, review }),
+        headers: { "Content-Type": "application/json" },
+      });
+      document.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
+  });
 });
 
 // adds event listener to each save delete button
-delBtn.forEach(btn => {
-    btn.addEventListener("click", () => {
-        myEl = btn.parentElement
-        // console.log(myEl.firstChild.nextSibling.firstChild.nextSibling.innerText)
-        id = (myEl.firstChild.nextSibling.firstChild.nextSibling.nextSibling.nextSibling.innerText)
-        console.log(id)
-    })
+delBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    myEl = btn.parentElement;
+    id = myEl.querySelector(".movieId").innerText;
+    console.log(id);
+  });
 });
+
 
 // TODOS: Save Movie
 // 1. get the id of the correct element
