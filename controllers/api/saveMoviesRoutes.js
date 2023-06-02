a6a9f3ea40b5f0b2b64455624a01b184badcf60d
Brent <124935518+Brent-Bew-Canon@users.noreply.github.com>
2023-06-01 19:45:14 -0700

    Delete movie functioning

--- controllers/api/saveMoviesRoutes.js
+++ controllers/api/saveMoviesRoutes.js
@@ -2,6 +2,10 @@ const router = require('express').Router();
 const { SavedMovies, MovieList, User } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+router.get('/', async (req, res) => {
+    res.status(200).json('hi')
+})
+
 router.post('/', async (req, res) => {
     try {
         //use the id to find the correct movie from the movielist
@@ -31,17 +35,17 @@ router.delete('/', withAuth, async (req, res) => {
         const saveMovieData = await SavedMovies.destroy({
             where: {
                 user_id: req.session.user_id,
-                movie_id: req.params.id,
+                movie_id: req.body.id,
             },
         });
 
-        if (!saveMovieData) {
-            res.status(404).json({ message: 'No project found with this id!' });
-            return;
-        }
+        // if (!saveMovieData) {
+        //     res.status(404).json({ message: 'No project found with this id!' });
+        //     return;
+        // }
 
-        res.status(200).json(saveMovieData);
-        console.log(myMovie)
+        // res.status(200).json();
+        // console.log(myMovie)
     } catch (err) {
         res.status(500).json(err);
         console.log(err)
