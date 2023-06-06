306fbf19dbc45e539ce05a26f282ce61a8a5e3c0
Brent <124935518+Brent-Bew-Canon@users.noreply.github.com>
2023-06-05 20:11:16 -0700

    Delete Route - functions properly with modal

--- public/js/display.js
+++ public/js/display.js
@@ -22,25 +22,3 @@ movBtn.addEventListener("click", async function (event) {
         console.log(error)
     }
 });
-
-// //Original before modal stuff
-// movBtn.forEach(btn => {
-//     btn.addEventListener("click", async function () {
-//         myEl = btn.parentElement.parentElement.previousSibling.previousSibling
-//         console.log(myEl)
-//         id = (myEl.firstChild.nextSibling.firstChild.nextSibling.nextSibling.nextSibling.innerText)
-//         console.log(id)
-
-//         try {
-//             response = await fetch('/api/save', {
-//                 method: 'POST',
-//                 body: JSON.stringify({ id }),
-//                 headers: { 'Content-Type': 'application/json' },
-//             });
-//             document.location.reload()
-
-//         } catch (error) {
-//             console.log(error)
-//         }
-//     })
-// });
