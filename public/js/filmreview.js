fc7f18ec8d14dcad3f9739ca55f0dcb1853cab93
Brent <124935518+Brent-Bew-Canon@users.noreply.github.com>
2023-06-05 20:55:45 -0700

    reload page when review movie

--- public/js/filmreview.js
+++ public/js/filmreview.js
@@ -1,24 +1,24 @@
-document.getElementById('reviewForm').addEventListener('submit', async function(event) {
-    event.preventDefault();
-  
-    const movieName = document.getElementById('movieName').value;
-    const review = document.getElementById('review').value;
-  
-    const response = await fetch('/api/review', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        movieName: movieName,
-        review: review,
-      }),
-    });
-  
-    if (response.ok) {
-      console.log('Successfully submitted review');
-    } else {
-      console.error('Error submitting review:', response.statusText);
-    }
+document.getElementById('reviewForm').addEventListener('submit', async function (event) {
+  event.preventDefault();
+
+  const movieName = document.getElementById('movieName').value;
+  const review = document.getElementById('review').value;
+
+  const response = await fetch('/api/review', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      movieName: movieName,
+      review: review,
+    }),
   });
-  
+  document.location.reload()
+
+  if (response.ok) {
+    console.log('Successfully submitted review');
+  } else {
+    console.error('Error submitting review:', response.statusText);
+  }
+});
