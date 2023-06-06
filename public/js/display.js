d3a875726f01130e4206fb7cd03a6cd97212a87c
Brent <124935518+Brent-Bew-Canon@users.noreply.github.com>
2023-06-05 19:48:54 -0700

    Modal - populates correct movie id

--- public/js/display.js
+++ public/js/display.js
@@ -2,21 +2,21 @@ const movBtn = document.querySelectorAll("#saveMovie")
 let id = 0
 movBtn.forEach(btn => {
     btn.addEventListener("click", async function () {
-        myEl = btn.parentElement
-        // console.log(myEl.firstChild.nextSibling.firstChild.nextSibling.innerText)
+        myEl = btn.parentElement.parentElement.previousSibling.previousSibling
+        console.log(myEl)
         id = (myEl.firstChild.nextSibling.firstChild.nextSibling.nextSibling.nextSibling.innerText)
         console.log(id)
 
-        try {
-            response = await fetch('/api/save', {
-                method: 'POST',
-                body: JSON.stringify({ id }),
-                headers: { 'Content-Type': 'application/json' },
-            });
-            document.location.reload()
+        // try {
+        //     response = await fetch('/api/save', {
+        //         method: 'POST',
+        //         body: JSON.stringify({ id }),
+        //         headers: { 'Content-Type': 'application/json' },
+        //     });
+        //     document.location.reload()
 
-        } catch (error) {
-            console.log(error)
-        }
+        // } catch (error) {
+        //     console.log(error)
+        // }
     })
 });
